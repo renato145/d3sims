791da18148d3702da39a503a3e01e4da63ff55c3
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2019-12-22 14:03:17 +1030

    lightbulb

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,5 @@
 import { drawBattery } from './battery';
+import { drawLightbulb } from './lightbulb';
 
 export const drawEnvironment = (selection, props) => {
   const { width, height, margin, batteryLevel } = props;
@@ -15,16 +16,26 @@ export const drawEnvironment = (selection, props) => {
       .attr('height', innerHeight)
       .attr('rx', 20);
 
+  // Battery
   const battery = selection.selectAll('.battery-container').data([null]);
   const batteryEnter = battery.enter().append('g')
       .attr('transform', 'translate(30,130)')
       .attr('class', 'battery-container');
-
   batteryEnter.merge(battery)
       .call(drawBattery, {
         batteryLevel,
         width: innerWidth*0.15,
         height: innerHeight*0.7
+      });
+
+  // Lightbulb
+  const lightbulb = selection.selectAll('.lightbulb-container').data([null]);
+  const lightbulbEnter = lightbulb.enter().append('g')
+      .attr('transform', 'translate(30,130)')
+      .attr('class', 'lightbulb-container');
+  lightbulbEnter.merge(lightbulb)
+      .call(drawLightbulb, {
+        type: 'classic'
       });
 
 };
